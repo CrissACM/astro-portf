6ed6d208e7d6d16adc1da4423375029ae4a0603a
crissacm <alex.ccm7Z@gmail.com>
2024-08-19 23:17:06 -0400

    update 2

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,31 +1,37 @@
-import { skills } from '../data/en'
+import { skills } from "../data/en";
 
-function Skills () {
+function Skills() {
   return (
     <div
-      id='skills'
-      className='flex flex-col justify-center relative z-[1] items-center'
+      id="skills"
+      className="relative z-[1] flex flex-col items-center justify-center"
     >
-      <div className='relative flex justify-between items-center flex-col w-full max-w-[1100px] gap-3 md:flex-col'>
-        <div className='text-[42px] text-center font-semibold text-dark dark:text-light mt-5 md:text-[32px] md:mt-3'>
+      <div className="relative flex w-full max-w-[1100px] flex-col items-center justify-between gap-3 md:flex-col">
+        <div className="mt-5 text-center text-[42px] font-semibold text-dark md:mt-3 md:text-[32px] dark:text-light">
           Skills
         </div>
-        <div className='text-lg text-center max-w-[600px] text-dark dark:text-light/70 md:text-base'>
+        <div className="max-w-[600px] text-center text-lg text-dark md:text-base dark:text-light/70">
           Here are some of my skills I've been working on.
         </div>
-        <div className='w-full flex flex-wrap gap-[30px] justify-center mt-[30px]'>
+        <div className="mt-[30px] flex w-full flex-wrap justify-center gap-[30px]">
           {skills.map((skill, index) => (
-            <div className='w-full max-w-[500px] border-primary dark:border-primaryDark border-[0.1px] border-solid shadow-[rgba(23,92,230,0.15)_0px_4px_24px] px-9 py-[18px] rounded-2xl md:max-w-[400px] md:px-9 md:py-2.5 xs:max-w-[330px] xs:px-9 xs:py-2.5' key={index}>
-              <h2 className='text-[28px] font-semibold text-dark dark:text-light text-center mb-5'>
+            <div
+              className="w-full max-w-[500px] rounded-2xl border-[0.1px] border-solid border-primary px-9 py-[18px] shadow-[rgba(23,92,230,0.15)_0px_4px_24px] md:max-w-[400px] md:px-9 md:py-2.5 xs:max-w-[330px] xs:px-9 xs:py-2.5 dark:border-primaryDark"
+              key={index}
+            >
+              <h2 className="mb-5 text-center text-[28px] font-semibold text-dark dark:text-light">
                 {skill.title}
               </h2>
-              <div className='flex justify-center flex-wrap gap-3 mb-5'>
-                {skill.skills.map(item => (
-                  <div className='text-base font-normal text-dark dark:text-light border flex items-center justify-center gap-2 px-4 py-3 rounded-xl border-solid border-dark/70 dark:border-light/70 md:text-sm md:px-3 md:py-2 xs:text-sm xs:px-3 xs:py-1.5' key={item.image}>
+              <div className="mb-5 flex flex-wrap justify-center gap-3">
+                {skill.skills.map((item) => (
+                  <div
+                    className="flex items-center justify-center gap-2 rounded-xl border border-solid border-dark/70 px-4 py-3 text-base font-normal text-dark md:px-3 md:py-2 md:text-sm xs:px-3 xs:py-1.5 xs:text-sm dark:border-light/70 dark:text-light"
+                    key={item.image}
+                  >
                     <img
                       src={item.image}
                       alt={item.name}
-                      className='w-8 h-8 sm:w-6 sm:h-6'
+                      className="h-8 w-8 sm:h-6 sm:w-6"
                     />
                     {item.name}
                   </div>
@@ -36,7 +42,8 @@ function Skills () {
         </div>
       </div>
     </div>
-  )
+  );
 }
+//prueba
 
-export default Skills
+export default Skills;
